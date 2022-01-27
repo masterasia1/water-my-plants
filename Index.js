e4ce9a37bd03ead311f09296fc415e20eb12cbc1
masterasia1 <a.mayfield18@gmail.com>
2022-01-26 19:59:04 -0800

    am1

--- Index.js
+++ Index.js
@@ -1,1 +1,7 @@
+const server = require('./Server')
+
+const PORT = process.env.PORT || 3300;
+server.listen(PORT, () => {
+  console.log(`listening`);
+});
 
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// do not make changes to this file
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  seeds: { directory: './data/seeds' },
+
+}
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: './data/auth.db3' },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/test.db3' },
+  },
+  production: {
+    ...sharedConfig,
+   
+    connection: process.env.DATABASE_URL,
+}
+}
